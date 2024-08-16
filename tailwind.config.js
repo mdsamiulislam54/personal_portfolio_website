6540269a70a9a15c1b748f7272fdf7db3fd5ff2e
mdsamiulislam54 <samiulm5332@gmail.com>
2024-08-17 00:58:50 +0600

    fast init

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+
+
+  /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    width: {
+      'laptop': '1024px',
+    },
+    colors: {
+      'p-blue': '#5E3BEE',
+      'p-red': '#E62872',
+      'p-white': '#FFF',
+      'p-transparent':' transparent',
+      'banner_bg':'#F5FCFF'
+    },
+    fontFamily:{
+      'robo' : "Roboto",
+    },
+ 
+  },
+  plugins: [],
+}
